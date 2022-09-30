656437004d0708b097e8c15090bad16fb440ca90
George Boorman <george.boorman@datacamp.com>
2022-09-30 16:27:18 +0100

    Update csv step in data_split

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
                 parameters={
-                    "input": "clean_sample:latest",
+                    "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
                     "random_seed": config["modeling"]["random_seed"],
                     "stratify_by": config["modeling"]["stratify_by"]
