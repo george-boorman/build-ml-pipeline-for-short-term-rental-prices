88322a9945d06eeb40ea865def894544c3f41ffb
George Boorman <george.boorman@datacamp.com>
2022-10-01 14:37:49 +0100

    Add boundary limits in basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Set boundary limits
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save updated DataFrame
     df.to_csv("clean_sample.csv", index=False)
 
